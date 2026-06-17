90a2e090ac4a52904feedb1f3aa884f9494ff7d2
JoshavEngelen <josha@mixnmac.nl>
2026-06-17 18:31:28 +0200

    Set placeholder routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Test route to check if Redis is working
 Route::get('/redis-test', function () {
     Cache::put('hello', 'world', 60);
 
     return Cache::get('hello');
+});
+
+Route::prefix('v1')->group(function () {
+
+    Route::post('/secrets', function () {
+        return [];
+    });
+
+    Route::get('/secrets/{token}', function () {
+        return [];
+    });
+
 });
