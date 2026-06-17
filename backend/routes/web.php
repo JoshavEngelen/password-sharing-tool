b4ee130046966dd37ffb09a4b692175931421b14
JoshavEngelen <josha@mixnmac.nl>
2026-06-17 18:25:57 +0200

    Redis connected to backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redis-test', function () {
+    Cache::put('hello', 'world', 60);
+
+    return Cache::get('hello');
 });
